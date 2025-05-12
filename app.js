388f64d56b893c0aafcfd2ac4af2a0836e6e9747
PedroMaravelli <phmaravelli123@gmail.com>
2025-05-11 21:46:11 -0300

    create routes alunos

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const app = express()
 const port = 3000
 const AdminRoutes = require("./Routes/Admin")
 const ProfessorRoutes = require("./Routes/Professor")
+const AlunoRoutes = require("./Routes/Aluno")
+
 
 
 app.use(express.json());
 app.use('/admin', AdminRoutes );
 app.use('/prof', ProfessorRoutes );
+app.use('/aluno', AlunoRoutes );
+
 
 
 app.listen(port, () => {
