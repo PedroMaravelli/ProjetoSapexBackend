d683595bfb025c779b3206cc13e6e231e55445b2
PedroMaravelli <phmaravelli123@gmail.com>
2025-08-26 22:35:48 -0300

    retirando arquivo index.js do gitignore

---
+++ src/database/models/index.js
@@ -0,0 +1,34 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const path = require('path');
+
+// Conexão com o banco de dados
+const sequelize = new Sequelize('mydb', 'root', 'Peixe123!', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: false,
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Importando os modelos
+db.Aluno = require('./Aluno')(sequelize, DataTypes);
+db.Professor = require('./Professor')(sequelize, DataTypes);
+db.Localizacao = require('./Localizacao')(sequelize, DataTypes);
+db.Trabalho = require('./Trabalho')(sequelize, DataTypes);
+db.Admin = require('./Admin')(sequelize, DataTypes);
+db.GuiaSapex = require('./GuiaSapex')(sequelize, DataTypes);
+db.AdminHasTrabalho = require('./AdminHasTrabalho')(sequelize, DataTypes);
+db.AdminHasGuiaSapex = require('./AdminHasGuiaSapex')(sequelize, DataTypes);
+db.AlunoHasTrabalho = require('./AlunoHasTrabalho')(sequelize, DataTypes);
+
+
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+module.exports = db;
