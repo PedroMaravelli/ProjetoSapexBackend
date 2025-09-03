0a213edf889875a8b4cb54d8f400b60a34feb786
Pellizonlc <arthurlc000999@gmail.com>
2025-09-02 21:53:39 -0300

    teste pipeline 2

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const AdminController = require('../controllers/Admin/adminController');  
-const cadastroTrabalhosMiddleware = require("../Middlawares/Admin/cadastroTrabalhosMiddleware")
-const cadastroGuiaSapexMiddleware = require("../Middlawares/Admin/cadastroGuiaSapexMiddleware")
+// const cadastroTrabalhosMiddleware = require("../Middlawares/Admin/cadastroTrabalhosMiddleware")
+// const cadastroGuiaSapexMiddleware = require("../Middlawares/Admin/cadastroGuiaSapexMiddleware")
 
-const {cadastroTrabalhosAdminSchema} = require("../Middlawares/schemas/adminSchemaValidation")
-const {cadastroGuiaSapexSchema} = require("../Middlawares/schemas/adminSchemaValidation")
-
-
+// const {cadastroTrabalhosAdminSchema} = require("../Middlawares/schemas/adminSchemaValidation")
+// const {cadastroGuiaSapexSchema} = require("../Middlawares/schemas/adminSchemaValidation")
 
 
-router.post('/cadastrotrabalhos',cadastroTrabalhosMiddleware(cadastroTrabalhosAdminSchema), AdminController.CadastroTrabalhos);
-router.post('/guia/cadastro',cadastroGuiaSapexMiddleware(cadastroGuiaSapexSchema), AdminController.CadastroInstrucao);
+
+
+router.post('/cadastrotrabalhos', AdminController.CadastroTrabalhos);
+router.post('/guia/cadastro', AdminController.CadastroInstrucao);
 router.post('/cadastrolocalizacao', AdminController.CadastroLocalizacao);
 
 
