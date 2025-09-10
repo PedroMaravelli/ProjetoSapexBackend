caf5bef9243fdb9e9357e0b1acd15fd8064cd444
PedroMaravelli <phmaravelli123@gmail.com>
2025-09-09 23:14:53 -0300

    fix: banco de dados prod

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,19 +1,30 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const path = require('path');
+require('dotenv').config(); // Para ler o .env
 
-// Conexão com o banco de dados
-const sequelize = new Sequelize('mydb', 'root', 'Peixe123!', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// Importando os modelos
+// Importando modelos
 db.Aluno = require('./Aluno')(sequelize, DataTypes);
 db.Professor = require('./Professor')(sequelize, DataTypes);
 db.Localizacao = require('./Localizacao')(sequelize, DataTypes);
@@ -24,11 +35,11 @@ db.AdminHasTrabalho = require('./AdminHasTrabalho')(sequelize, DataTypes);
 db.AdminHasGuiaSapex = require('./AdminHasGuiaSapex')(sequelize, DataTypes);
 db.AlunoHasTrabalho = require('./AlunoHasTrabalho')(sequelize, DataTypes);
 
-
+// Executando associações
 Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 module.exports = db;
