491a4703331027a12d1c3b57ceb02f1379940d39
Pedro Maravelli <pedro@justisecure.com.br>
2025-09-19 17:36:07 -0300

    feat: adicionando cadastro de trabalhos em lote por planilha excel

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 const EmailHelper = require('./EmailHelper');
+const formatXlsxToJsonHelper = require('./formatXlsxToJsonHelper');
 const ResponseHelper = require('./ResponseHelper');
 
 module.exports = {
   ResponseHelper,
-  EmailHelper
+  EmailHelper,
+  formatXlsxToJsonHelper
 };
