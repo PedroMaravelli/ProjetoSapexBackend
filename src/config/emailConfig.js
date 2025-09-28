0323924b80e635bfc49814b05843edddd1394589
PedroMaravelli <phmaravelli123@gmail.com>
2025-09-27 23:09:38 -0300

    fix: ajustando config de email para o render

--- src/config/emailConfig.js
+++ src/config/emailConfig.js
@@ -4,12 +4,15 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
-    host: process.env.HOST_EMAIL,
-    port: 587,
-    secure: false,
-auth: {
-    user: process.env.USER_EMAIL,
-    pass: process.env.PASSWORD_EMAIL,
+    host: process.env.HOST_EMAIL || "smtp.gmail.com",
+    port: process.env.EMAIL_PORT || 587,
+    secure: process.env.EMAIL_SECURE === 'true' || false,
+    connectionTimeout: 60000,
+    greetingTimeout: 30000,
+    socketTimeout: 60000,
+    auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.PASSWORD_EMAIL,
     },
 });
 
