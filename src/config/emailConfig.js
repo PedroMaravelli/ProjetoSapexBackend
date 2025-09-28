7d072607068f7057e6e4ab7589a0ca0cb4865ada
PedroMaravelli <phmaravelli123@gmail.com>
2025-09-28 00:22:56 -0300

    fix: removendo timedout

--- src/config/emailConfig.js
+++ src/config/emailConfig.js
@@ -7,9 +7,6 @@ const transporter = nodemailer.createTransport({
     host: process.env.HOST_EMAIL || "smtp.gmail.com",
     port: process.env.EMAIL_PORT || 587,
     secure: process.env.EMAIL_SECURE === 'true' || false,
-    connectionTimeout: 60000,
-    greetingTimeout: 30000,
-    socketTimeout: 60000,
     auth: {
         user: process.env.USER_EMAIL,
         pass: process.env.PASSWORD_EMAIL,
